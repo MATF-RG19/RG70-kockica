0dd7ecb31390ef0b140c09ba2197070bdb4ec6cb
IlicMatija <lolyepreferral@gmail.com>
2019-12-26 18:05:16 +0100

    Napravljena kocka

---
+++ Makefile
@@ -0,0 +1,22 @@
+PROGRAM   = zadatak
+CC        = gcc
+CFLAGS    = -g -Wall
+LDFLAGS   = -lGL -lGLU -lglut -lm
+
+$(PROGRAM): skelet.o
+	$(CC) -o $(PROGRAM) skelet.o $(LDFLAGS)
+	
+
+.PHONY: clean dist
+
+clean:
+	-rm -f *.o $(PROGRAM) *core
+
+dist: clean
+	-tar -chvj -C .. -f ../$(PROGRAM).tar.bz2 $(PROGRAM)
+
+
+
+
+run:
+	./$(PROGRAM)
---
+++ skelet.c
@@ -0,0 +1,147 @@
+#include <GL/glut.h>
+#include <stdio.h>
+#include <math.h>
+
+#define TIMER_INTERVAL 20
+#define TIMER_ID 0
+
+static void on_display();
+static void on_reshape(int width, int height);
+static void on_keyboard(unsigned char key, int x, int y);
+static void on_timer(int id);
+
+static void draw_axis(float len);
+static void draw_cube();
+
+float animation_parameter = 0;
+int animation_ongoing = 0;
+
+
+int main(int argc, char **argv)
+{
+    /* Inicijalizuje se GLUT. */
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+
+    /* Kreira se prozor. */
+    glutInitWindowSize(800, 800);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow(argv[0]);
+
+    /* Registruju se callback funkcije */
+    glutDisplayFunc(on_display);
+    glutReshapeFunc(on_reshape);
+    glutKeyboardFunc(on_keyboard);
+
+    /* Obavlja se OpenGL inicijalizacija. */
+    glEnable(GL_DEPTH_TEST);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+
+    float light_position[] = {0, 10, 20, 0};
+//     float light_ambient[] = {.3f, .3f, .3f, 1};
+//     float light_diffuse[] = {.7f, .7f, .7f, 1};
+//     float light_specular[] = {.7f, .7f, .7f, 1};
+// 
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+//     glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+//     glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+//     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+
+    glClearColor(.75,.75,.75,0);
+ 
+    glutMainLoop();
+
+  return 0;
+}
+
+/*
+ *POMOCNA FUNKCIJA ZA ISCRTAVANJE KOODINATNOG SISTEMA
+ */
+void draw_axis(float len) {
+    glDisable(GL_LIGHTING);
+
+    glBegin(GL_LINES);
+        glColor3f(1,0,0);
+        glVertex3f(0,0,0);
+        glVertex3f(len,0,0);
+
+        glColor3f(0,1,0);
+        glVertex3f(0,0,0);
+        glVertex3f(0,len,0);
+
+        glColor3f(0,0,1);
+        glVertex3f(0,0,0);
+        glVertex3f(0,0,len);
+    glEnd();
+
+    glEnable(GL_LIGHTING);
+}
+
+void on_keyboard(unsigned char key, int x, int y) {
+    switch(key) {
+        
+        case 27:
+          exit(0);
+          break;
+    }
+}
+
+void on_timer(int id) {
+    if (id == TIMER_ID) {
+     
+    }
+
+    glutPostRedisplay();
+
+    if(animation_ongoing)
+        glutTimerFunc(TIMER_INTERVAL,on_timer,TIMER_ID);
+   
+}
+
+void on_reshape(int width, int height) {
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    gluPerspective(30, (float) width/height, 1, 1000);
+}
+
+void draw_cube(){
+    glPushMatrix();
+
+    glutSolidCube(1);
+
+    glPopMatrix();
+}
+
+
+void on_display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    
+    gluLookAt(10, 5, 0,
+              0, -1, 0,
+              0, 1, 0);
+
+
+    GLfloat diffuse[] = {1,1,1,0};
+
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
+
+    draw_axis(5);
+    glPushMatrix();
+        draw_cube();
+    glPopMatrix();
+
+
+    glutSwapBuffers();
+}
+
+
+
+
+
